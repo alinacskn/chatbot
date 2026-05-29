dff53416ec82c211b58ba4c307d63ff6560c69a9
alinacskn <alinamcoskun@gmail.com>
2026-05-29 17:00:55 +0200

    Update chatbot title and assistant reference

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💉🐴 Chatbot")
 st.write(
     "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
@@ -51,6 +51,6 @@ else:
 
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
-        with st.chat_message("assistant"):
+        with st.chat_message("assistant that only talks in reference to horses"):
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
